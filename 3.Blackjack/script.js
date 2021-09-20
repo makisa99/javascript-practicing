21f10963959d0ecce12f2a4fbfc97b533c5a64b0
makisa99 <72605856+makisa99@users.noreply.github.com>
2021-09-20 10:23:39 +0200

    blackjack-finished

--- 3.Blackjack/script.js
+++ 3.Blackjack/script.js
@@ -1,49 +1,73 @@
-let firstCard = Math.floor(Math.random() * 10 + 2);
-let secondCard = Math.floor(Math.random() * 10 + 2);
-let cards = [firstCard, secondCard];
-let hasBlackJack = false;
-let isAlive = true;
-let sum = firstCard + secondCard;
+let firstCard;
+let secondCard;
+let cards = [];
+let hasBlackJack;
+let isAlive;
+let sum = 0;
 let newCard;
 let message;
 
-let messageEl = document.getElementById("message-el");
+let messageEl = document.querySelector("#message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
 
-function checkSum() {
-  if (sum < 21) {
-    message = "Do you want to draw a new card?";
-    isAlive = true;
-  } else if (sum === 21) {
-    message = "YOU WON";
-    hasBlackJack = true;
-    isAlive = false;
-  } else {
-    message = "you lost";
-    isAlive = false;
-    return;
-  }
-  messageEl.textContent = message;
-}
+let player = {
+  name: "Mario",
+  chips: 145,
+};
+
+let playerEl = document.querySelector("#player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function startGame() {
+  isAlive = true;
+  hasBlackJack = false;
+  firstCard = getRandomCard();
+  secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = cards[0] + cards[1];
   renderGame();
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: "; //todo: render cards to be shown, 3:09;
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += " " + cards[i];
+  }
   sumEl.textContent = "Sum: " + sum;
-  checkSum();
+  if (sum < 21) {
+    message = "Do you want to draw a new card?";
+    console.log(sum);
+  } else if (sum === 21) {
+    message = "YOU WON";
+    hasBlackJack = true;
+    console.log(sum);
+  } else {
+    message = "you lost";
+    console.log(sum);
+    isAlive = false;
+  }
+  messageEl.textContent = message;
 }
 
 function newCardF() {
-  newCard = Math.floor(Math.random() * 10 + 2);
-  sum += newCard;
-  cards.push(newCard);
-  console.log(cards);
+  if (!hasBlackJack && isAlive) {
+    newCard = getRandomCard();
+    sum += newCard;
+    cards.push(newCard);
 
-  // cardsEl.textContent += " " + newCard;
-  // sumEl.textContent = "Sum: " + sum;
-  renderGame();
+    renderGame();
+  }
+}
+
+function getRandomCard() {
+  let randomnum = Math.floor(Math.random() * 13 + 1);
+  if (randomnum === 1) {
+    return 11;
+  } else if (randomnum > 10) {
+    return 10;
+  } else {
+    return randomnum;
+  }
+  return; //13=max+1-min, 1 = min
 }
