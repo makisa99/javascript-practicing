a61a2262b886cf4a312768d7deec7e5f635256c1
makisa99 <72605856+makisa99@users.noreply.github.com>
2021-09-20 13:23:31 +0200

    making views

---
+++ 4.ChromeExtension/script.js
@@ -0,0 +1,29 @@
+let myLeads = [];
+
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
+
+function renderLeads() {
+  var listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    // listItems += "<li><a target = '_blank' href = '" +myLeads[i] +"'>" +myLeads[i] +"</a></li>";
+    listItems += `
+        <li>
+            <a target = '_blank' href = '${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+    `;
+    //   const li = document.createElement("li");
+    //   li.textContent = myLeads[i];
+    //   ulEl.append(li);
+  }
+  ulEl.innerHTML = listItems;
+}
