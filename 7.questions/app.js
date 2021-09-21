8315ce9f931e107f364b7d6b8a9310a75ea77d0a
makisa99 <72605856+makisa99@users.noreply.github.com>
2021-09-22 00:31:22 +0200

    questions-finished

--- 7.questions/app.js
+++ 7.questions/app.js
@@ -1,2 +1,28 @@
 //using selectors inside the element
-// traversing the dom
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (qstn) {
+  const btn = qstn.querySelector(".question-btn");
+  //   console.log(btn);
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== qstn) {
+        item.classList.remove("show-text");
+      }
+    });
+    qstn.classList.toggle("show-text");
+  });
+});
+
+// // traversing the dom
+// let questionBtns = document.querySelectorAll(".question-btn");
+
+// questionBtns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     // console.log(
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//     // );
+//   });
+// });
