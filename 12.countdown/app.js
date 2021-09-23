8ed77309ee4115ca7d790ce0550133ef47ccd9d0
makisa99 <72605856+makisa99@users.noreply.github.com>
2021-09-23 12:30:24 +0200

    countdown-finished

--- 12.countdown/app.js
+++ 12.countdown/app.js
@@ -37,3 +37,41 @@ const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 
 giveaway.textContent = `givewaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+//future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  let days = t / oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`;
+  }
+}
+
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
