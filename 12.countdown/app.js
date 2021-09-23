d3cf848034b705fef9f6dc30de7cd10b6a207aca
makisa99 <72605856+makisa99@users.noreply.github.com>
2021-09-23 03:36:48 +0200

    initialized date

--- 12.countdown/app.js
+++ 12.countdown/app.js
@@ -21,3 +21,19 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2021, 10, 23, 5, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+const month = months[futureDate.getMonth()];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `givewaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
